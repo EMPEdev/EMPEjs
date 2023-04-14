5264b8ce43513879ff4a6b1b5f7094cb22e28d05
EMPEdev <130178528+EMPEdev@users.noreply.github.com>
2023-04-14 12:29:50 +0200

    js learning

---
+++ script.js
@@ -0,0 +1,26 @@
+let myVar = 87;
+
+myVar++;
+
+console.log(myVar);
+//
+let age = 20;
+let name = "Patryk";
+
+console.log(age);
+console.log(name);
+
+
+//change background button
+
+function bgColorChangeDark() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+};
+function bgColorChangeLight(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+
+};
